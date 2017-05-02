ae7b9d9c179b87e8152f440d515c149b18b54698
fuzhenn <fuzhen@maptalks.org>
2017-05-02 13:11:11 +0800

    0.4.1

--- dist/maptalks.heatmap.js
+++ dist/maptalks.heatmap.js
@@ -1,7 +1,10 @@
 /*!
- * maptalks.heatmap v0.4.0
+ * maptalks.heatmap v0.4.1
  * LICENSE : MIT
  * (c) 2016-2017 maptalks.org
+ */
+/*!
+ * requires maptalks@^0.23.0 
  */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('maptalks')) :
@@ -462,5 +465,7 @@ HeatLayer.registerRenderer('canvas', function (_maptalks$renderer$Ca) {
 exports.HeatLayer = HeatLayer;
 
 Object.defineProperty(exports, '__esModule', { value: true });
+
+typeof console !== 'undefined' && console.log('maptalks.heatmap v0.4.1, requires maptalks@^0.23.0.');
 
 })));
