28b86cc7b1d65100ba799ba06dab983d5c4c953c
fuzhenn <fuzhen@maptalks.org>
2022-08-18 17:35:34 +0800

    upgrade

--- karma.config.js
+++ karma.config.js
@@ -2,22 +2,26 @@ const pkg = require('./package.json');
 
 module.exports = function (config) {
     config.set({
-        basePath : '.',
+        browserDisconnectTimeout: 100000,
+        browserNoActivityTimeout: 100000,
+        browserDisconnectTolerance: 12,
+        frameworks: ['mocha', 'expect', 'expect-maptalks'],
+        basePath: '.',
         client: {
             mocha: {
                 timeout : 36000
             }
         },
-        frameworks: ['mocha', 'expect', 'expect-maptalks', 'happen'],
         files: [
             'node_modules/maptalks/dist/maptalks.js',
             pkg.main,
-            'test/**/*.js'
+            'test/test.js'
         ],
+        proxies: {
+        },
         preprocessors: {
         },
         browsers: ['Chrome'],
-        reporters: ['mocha'],
-        singleRun : true
-    })
+        reporters: ['mocha']
+    });
 };
